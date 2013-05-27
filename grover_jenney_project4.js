8edbcec2e7e88d75572043b16753cf3bb04d06c3
Jenney Grover <jenneygrover@fullsail.edu>
2013-05-26 23:59:29 -0400

    12 problems plus 1st code
    Added the 12 problems as comments and also added the first code.

--- grover_jenney_project4.js
+++ grover_jenney_project4.js
@@ -5,3 +5,30 @@
 //Project 4
 //May 30, 2013
 
+//strings
+
+//Does a string follow a 123-456-7890 pattern like a phone number? 
+//Does a string follow an aaa@bbb.ccc pattern like an email address? 
+//Is the string a URL? (Does it start with http: or https:?
+//Title-case a string (split into words, then uppercase the first letter of each word. 
+//Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a, b, c" + "," + "/" -> "a/b/c"
+
+//Numbers
+
+//Format a number to use a specific number of decimal places, as for one: 2.1 -> 2.10
+//fuzzy-match a number: is the number above or below a number within a certain percent? 
+//find the number of hours or days difference between two dates
+var myBirthday = new Date(1977, 03, 17); //April 17, 1977
+var todaysDate = new Date(2013, 04, 26); //May 26, 2013
+var elapsedTime = todaysDate - myBirthday; 
+//var myAge = elapsedTime.getFullYear(); 
+console.log(elapsedTime); 
+
+//given a string version of a number such as "42", return the value as ann actual Number, such as 42. 
+
+//arrays
+
+//find the smallest value in an array that is greater than a given number
+//find the total value of just the numbers in an array, even if some of the items are not numbers. 
+//given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2}, {a:3}, {a:1}] -> [{a:1}, {a:2}, {a:3}] 
+
